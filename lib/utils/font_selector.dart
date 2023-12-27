4ff03ef2dddd68e05fc9350e83fdde479a01dcfc
Faheem <faheemjawfar@gmail.com>
2023-12-27 21:27:18 +0530

    Added Font variant for each language

--- lib/utils/font_selector.dart
+++ lib/utils/font_selector.dart
@@ -49,24 +49,99 @@ class FontSelector {
     'Uzbek'
   ];
 
-
-  static TextStyle getFontStyle(String selectedLanguage){
+  static TextStyle getFontStyle(String selectedLanguage) {
     print(selectedLanguage);
-
-
-    if(selectedLanguage == 'Tamil'){
-      return GoogleFonts.muktaMalar(
-
-      );
+    switch (selectedLanguage) {
+      case 'Albanian':
+        return GoogleFonts.notoSans();
+      case 'Amazigh':
+        return GoogleFonts.notoSans();
+      case 'Arabic':
+        return GoogleFonts.notoSansArabic();
+      case 'Amharic':
+        return GoogleFonts.notoSansEthiopic();
+      case 'Azerbaijani':
+        return GoogleFonts.notoSans();
+      case 'Bengali':
+        return GoogleFonts.notoSansBengali();
+      case 'Bosnian':
+        return GoogleFonts.notoSans();
+      case 'Bulgarian':
+        return GoogleFonts.notoSans();
+      case 'Chinese':
+        return GoogleFonts.notoSansSc();
+      case 'Czech':
+        return GoogleFonts.notoSans();
+      case 'Divehi':
+        return GoogleFonts.notoSansThaana();
+      case 'Dutch':
+        return GoogleFonts.notoSans();
+      case 'English':
+        return GoogleFonts.notoSans();
+      case 'French':
+        return GoogleFonts.notoSans();
+      case 'German':
+        return GoogleFonts.notoSans();
+      case 'Hausa':
+        return GoogleFonts.notoSans();
+      case 'Hindi':
+        return GoogleFonts.notoSansDevanagari();
+      case 'Indonesian':
+        return GoogleFonts.notoSans();
+      case 'Italian':
+        return GoogleFonts.notoSans();
+      case 'Japanese':
+        return GoogleFonts.notoSansJp();
+      case 'Korean':
+        return GoogleFonts.notoSansNKo();
+      case 'Kurdish':
+        return GoogleFonts.notoSansArabic();
+      case 'Malay':
+        return GoogleFonts.notoSans();
+      case 'Malayalam':
+        return GoogleFonts.notoSansMalayalam();
+      case 'Norwegian':
+        return GoogleFonts.notoSans();
+      case 'Pashto':
+        return GoogleFonts.notoSansArabic();
+      case 'Persian':
+        return GoogleFonts.notoSansArabic();
+      case 'Polish':
+        return GoogleFonts.notoSans();
+      case 'Portuguese':
+        return GoogleFonts.notoSans();
+      case 'Romanian':
+        return GoogleFonts.notoSans();
+      case 'Russian':
+        return GoogleFonts.notoSans();
+      case 'Sindhi':
+        return GoogleFonts.notoSansArabic();
+      case 'Somali':
+        return GoogleFonts.notoSans();
+      case 'Spanish':
+        return GoogleFonts.notoSans();
+      case 'Swahili':
+        return GoogleFonts.notoSans();
+      case 'Swedish':
+        return GoogleFonts.notoSans();
+      case 'Tajik':
+        return GoogleFonts.notoSans();
+      case 'Tamil':
+        return GoogleFonts.notoSansTamil();
+      case 'Tatar':
+        return GoogleFonts.notoSans();
+      case 'Thai':
+        return GoogleFonts.notoSansThai();
+      case 'Turkish':
+        return GoogleFonts.notoSans();
+      case 'Urdu':
+        return GoogleFonts.notoSansArabic();
+      case 'Uyghur':
+        return GoogleFonts.notoSansArabic();
+      case 'Uzbek':
+        return GoogleFonts.notoSans();
+      default:
+        return GoogleFonts.notoSans();
     }
-
-    else if(selectedLanguage == 'Arabic'){
-      return GoogleFonts.notoSansArabic(
-
-      );
-    }
-
-    return GoogleFonts.notoSans();
-
   }
 }
